e56706fe2368b8798788cdc4f3cda458874501bf
Scott Smith <scottstephensmith@gmail.com>
2014-09-02 23:15:32 -0400

    initial commit
    needs map, interaction, and responsive menu

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$('.gallery').slick({
+      dots: true,
+      arrows:false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows:false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+});
+});
